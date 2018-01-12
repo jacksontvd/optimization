0f05273e708b65e72aa783f02b871e10cb0c6086
Jackson Tank Van Dyke <jayteeveedee@ln003.brc>
2018-01-11 19:22:36 -0800

    fill cov.sh file

--- covariance.sh
+++ covariance.sh
@@ -17,4 +17,13 @@ module load python/2.7.8
 module load numpy
 module load scipy
 module load cython
+python cluster.py covar e x
+python cluster.py covar e c
+python cluster.py covar e T
 python cluster.py covar e d
+python cluster.py covar x c
+python cluster.py covar x T
+python cluster.py covar x d
+python cluster.py covar c T
+python cluster.py covar c d
+python cluster.py covar T d
