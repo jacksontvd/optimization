d03185e31c262b6a594cedbc6586405efed09b47
jackson tvd <jacksontvd@jacksons-MacBook-Pro.local>
2018-04-08 20:49:17 -0700

    added test file

---
+++ sub_py/test.py
@@ -0,0 +1,30 @@
+import time
+from math import sqrt, pi
+import os, sys
+import numpy as np
+from scipy import optimize
+from scipy import integrate
+
+from ranges import *
+
+cwd = os.getcwd()
+
+real_par = param_ranges['98252'] 
+
+
+param_ranges['98252'] = [10.37,1.27,1.18,0.87,0.52]
+
+def test_error(Z, A, e ,x ,c ,T,d, generate_number, parsed_data, **kwargs):
+    error = 1
+    error += abs(e - 10.37)
+    error += abs(x - 1.27)
+    error += abs(c - 1.18)
+    error += abs(T - 0.87)
+    error += abs(d - 0.52)
+    #  error += np.exp(-abs(e -10.37)/10)
+    #  error += np.exp(-abs(x - 1.27)/10)
+    #  error += np.exp(-abs(c - 1.18)/10)
+    #  error += np.exp(-abs(T - 0.87)/10)
+    #  error += np.exp(-abs(d - 0.52)/10)
+    return error**2, error, None
+
