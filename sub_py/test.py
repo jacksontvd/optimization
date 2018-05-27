529e3f2f283829b99fe494dfbb2c288e8e7ddc1e
jackson tvd <jacksontvd@jacksons-MacBook-Pro.local>
2018-05-27 15:43:29 -0700

    new weights in range.py (new division)

--- sub_py/test.py
+++ sub_py/test.py
@@ -9,18 +9,18 @@ from ranges import *
 
 cwd = os.getcwd()
 
-#  parameters = param_list(98,252,"spontaneous")
+parameters = param_list(98,252,"spontaneous")
 
 def test_error(Z, A, e ,x ,c ,T,d, generate_number, parsed_data, **kwargs):
     error = 1
-    #  error += abs(e - 10.37)
-    #  error += abs(x - 1.27)
-    #  error += abs(c - 1.18)
-    #  error += abs(T - 0.87)
-    #  error += abs(d - 0.52)
-    error += np.exp(abs(e -10.37))
-    error += np.exp(abs(x - 1.27))
-    error += np.exp(abs(c - 1.18))
-    error += np.exp(abs(T - 0.87))
-    error += np.exp(abs(d - 0.52))
+    error += abs(e - 10.37)
+    error += abs(x - 1.27)
+    error += abs(c - 1.18)
+    error += abs(T - 0.87)
+    error += abs(d - 0.52)
+    #  error += np.exp(abs(e -10.37))
+    #  error += np.exp(abs(x - 1.27))
+    #  error += np.exp(abs(c - 1.18))
+    #  error += np.exp(abs(T - 0.87))
+    #  error += np.exp(abs(d - 0.52))
     return error**2, error, None, None,None,None,10
