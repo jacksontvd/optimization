70cda6a65c560de71b892abb8c26d7e349d239d5
jackson tvd <jacksontvd@jacksons-MacBook-Pro.local>
2018-05-29 09:39:52 -0700

    print h in covmatrix.py

--- sub_py/covmatrix.py
+++ sub_py/covmatrix.py
@@ -34,6 +34,7 @@ def log_probability(chi_sq,dof):
 def freya_hessian(Z,A,generate_number,h,reac_t):
     parameters = param_list(Z,A,reac_t)
     print("Using Hessian to calculate correlation matrix at the point: ",parameters)
+    print("Interval used to estimate derivatives: ",h)
     parsed_data = data_parse(Z,A,reac_t)
     def objective(parameters):
         error_array = error(Z, A, parameters[0],parameters[1], parameters[2], parameters[3], parameters[4], generate_number, parsed_data, reaction_type = reac_t)
